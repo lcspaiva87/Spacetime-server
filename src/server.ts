f08212a6254e7a477d840b4d285dd2778a8eeff7
lcspaiva <lcspaiva87@gmail.com>
2023-05-19 15:44:00 -0300

    fix: start project

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
+
+const app = fastify()
+app.register(cors, { origin: true }) // full system open
+app.register(memoriesRoutes)
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333 🚀')
+  })
